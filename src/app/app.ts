66a4deec6318279d26d82c607ce0d588511f3aac
Julian Matschinske <julian.matschinske@bitspark.de>
2018-10-23 18:43:13 +0200

    Restructure with app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,42 +1,5 @@
 import '../styles/index.scss';
+import {AppComponent} from "./components/app";
 
-import {LandscapeModel} from "./model/landscape";
-import {BlueprintModel, BlueprintType} from "./model/blueprint";
-import {LandscapeComponent} from "./components/landscape";
-import {StorageComponent} from "./components/storage";
-import {ApiService} from "./services/api";
-
-const landscape = new LandscapeModel();
-new LandscapeComponent(landscape, 'map');
-const storageComponent = new StorageComponent(landscape, new ApiService('http://localhost:5149/'));
-
-
-// JUST FOR TESTING:
-
-document.getElementById('sl-btn-add-bp')!.addEventListener('click', () => {
-    const name = (document.getElementById('sl-bp-name') as any).value;
-    landscape.addBlueprint(new BlueprintModel(name, BlueprintType.Local));
-});
-
-landscape.subscribeSelectionChanged(bp => {
-    document.getElementById('sl-bp-selected')!.textContent = bp === null ? 'null' : bp.getFullName();
-});
-
-landscape.subscribeBlueprintAdded(bp => {
-    const bpel = document.createElement('li');
-    bpel.textContent = bp.getFullName();
-    bpel.addEventListener('click', () => {
-        bp.select();
-    });
-    document.getElementById('sl-bps')!.append(bpel);
-
-    bp.subscribeSelectChanged(selected => {
-        if (selected) {
-            bpel.style.color = 'red';
-        } else {
-            bpel.style.color = 'inherit';
-        }
-    })
-});
-
-storageComponent.load();
+const app = new AppComponent('app', 'http://localhost:5149/');
+app.start();
---
+++ src/app/components/app.ts
@@ -0,0 +1,23 @@
+import {LandscapeModel} from "../model/landscape";
+import {StorageComponent} from "./storage";
+import {ApiService} from "../services/api";
+import {LandscapeComponent} from "./landscape";
+
+export class AppComponent {
+    private readonly landscapeModel: LandscapeModel;
+    private landscapeComponent: LandscapeComponent;
+    private storageComponent: StorageComponent;
+
+    constructor(id: string, host: string) {
+        this.landscapeModel = new LandscapeModel();
+        this.landscapeComponent = new LandscapeComponent(this.landscapeModel, id);
+        this.storageComponent = new StorageComponent(this.landscapeModel, new ApiService(host));
+    }
+
+    public async start(): Promise<void> {
+        return new Promise<void>(async resolve => {
+            await this.storageComponent.load();
+            resolve();
+        });
+    }
+}
