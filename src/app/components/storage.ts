16407debd5704986b69aff41320a15d0945c3e82
Taleh Didover <taleh.didover@bitspark.de>
2018-10-23 18:00:23 +0200

    Remove unnecessary subject from Storage

--- src/app/components/storage.ts
+++ src/app/components/storage.ts
@@ -1,11 +1,8 @@
 import {LandscapeModel} from "../model/landscape";
 import {BlueprintModel} from "../model/blueprint";
 import {ApiService} from "../services/api";
-import {Subject} from "rxjs";
 
 export class StorageComponent {
-    private blueprintsLoaded = new Subject<Array<BlueprintModel>>();
-
     constructor(private landscape: LandscapeModel, private api: ApiService) {
 
     }
@@ -16,9 +13,5 @@ export class StorageComponent {
             bpList.forEach(bp => this.landscape.addBlueprint(bp));
             resolve();
         });
-    }
-
-    public subscribeBlueprintsLoaded(cb: (_: Array<BlueprintModel>) => void): void {
-        this.blueprintsLoaded.subscribe(cb);
     }
 }
