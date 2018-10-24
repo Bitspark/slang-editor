775cc400ac11aaf23fd60c37466611621b41ba8a
Taleh Didover <taleh.didover@bitspark.de>
2018-10-24 11:30:11 +0200

    Open doubleClicked blueprint

--- src/app/components/app.ts
+++ src/app/components/app.ts
@@ -2,13 +2,14 @@ import {LandscapeModel} from "../model/landscape";
 import {StorageComponent} from "./storage";
 import {ApiService} from "../services/api";
 import {LandscapeComponent} from "./landscape";
+import {BlueprintComponent} from "./blueprint";
 
 export class AppComponent {
     private readonly landscapeModel: LandscapeModel;
     private landscapeComponent: LandscapeComponent;
     private storageComponent: StorageComponent;
 
-    constructor(id: string, host: string) {
+    constructor(private id: string, host: string) {
         this.landscapeModel = new LandscapeModel();
         this.landscapeComponent = new LandscapeComponent(this.landscapeModel, id);
         this.storageComponent = new StorageComponent(this.landscapeModel, new ApiService(host));
@@ -16,10 +17,11 @@ export class AppComponent {
     }
     
     private subscribe(): void {
+        const that = this;
         this.landscapeModel.subscribeBlueprintAdded(blueprint => {
             blueprint.subscribeOpenedChanged(opened => {
                 if (opened) {
-                    alert(`Open blueprint ${blueprint.getFullName()}`);
+                    new BlueprintComponent(blueprint, that.id);
                 }
             });
         });
--- src/app/components/blueprint.ts
+++ src/app/components/blueprint.ts
@@ -1,8 +1,36 @@
 import {BlueprintModel} from '../model/blueprint';
+import {dia, shapes} from "jointjs";
+import {redirectPaperEvents} from "./utils";
 
 export class BlueprintComponent {
+    private graph = new dia.Graph();
+    private paper: dia.Paper;
 
-    constructor(private blueprint: BlueprintModel) {
+    constructor(private blueprint: BlueprintModel, private id: string) {
+        const elem = document.getElementById(id);
+        const newElem = document.createElement('div');
+        const p = elem!.parentElement;
+        newElem.id = id;
+        elem!.remove();
+        p!.appendChild(newElem);
+
+
+        this.paper = new dia.Paper({
+            el: document.getElementById(id),
+            model: this.graph,
+            width: 600,
+            height: 600,
+            gridSize: 10,
+            drawGrid: {
+                name: "fixedDot",
+                color: "#000000",
+            },
+            background: {
+                color: '#d7dcf2',
+            }
+        });
+
+        redirectPaperEvents(this.paper);
     }
 
 }
