62ba46314875d765b5fde2d5977f5729de231b00
Julian Matschinske <julian.matschinske@bitspark.de>
2018-10-26 19:02:37 +0200

    Change zoom factors

--- src/app/ui/cavas.ts
+++ src/app/ui/cavas.ts
@@ -67,7 +67,7 @@ export class Canvas {
         });
     }
 
-    private addZooming(speed = 0.1, min = .8, max = 3) {
+    private addZooming(speed = 0.1, min = .5, max = 2.5) {
         const paper = this.paper;
         const zoom = function (x: number, y: number, delta: number) {
             const scale = paper.scale();
