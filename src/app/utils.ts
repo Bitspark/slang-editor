02fc0a0067ab618b6e1a41450f5286be840a8c8d
Taleh Didover <taleh.didover@bitspark.de>
2018-10-26 18:14:02 +0200

    Make ports triangle shaped to indicate direction

--- src/app/utils.ts
+++ src/app/utils.ts
@@ -1,10 +1,39 @@
-import {dia, shapes} from "jointjs";
+import {attributes, dia, shapes} from "jointjs";
 import {OperatorModel} from "./model/operator";
 import {BlueprintModel} from "./model/blueprint";
 import Port = dia.Element.Port;
 import {PortModel, PortType} from "./model/port";
+import SVGAttributes = attributes.SVGAttributes;
 
 export class JointJSElements {
+    private static inPortMarkup: string = "<path class='sl-port sl-port-in' d=''></path>";
+    private static outPortMarkup: string = "<path class='sl-port sl-port-out' d=''></path>";
+
+    private static inPortAttrs: SVGAttributes = {
+        paintOrder: "stroke fill",
+        d: "M 0 0 L 10 0 L 5 8 z",
+        transform: "translate(0,-3)",
+        magnet: true,
+        stroke: "black",
+        strokeWidth: 1,
+    };
+    private static outPortAttrs = JointJSElements.inPortAttrs;
+
+    private static blueprintAttrs: SVGAttributes = {
+        fillOpacity: .2,
+        stroke: "black",
+        strokeWidth: 1,
+        rx: 6,
+        ry: 6,
+    };
+
+    private static operatorAttrs = Object.assign(
+        JointJSElements.blueprintAttrs, {
+            fillOpacity: 1,
+        }
+    );
+
+
     private static createPortItems(group: string, port: PortModel): Array<Port> {
         let portItems: Array<Port> = [];
 
@@ -25,6 +54,11 @@ export class JointJSElements {
             default:
                 portItems.push({
                     group: group,
+                    attrs: {
+                        ".sl-port": {
+                            fill: "cyan",
+                        }
+                    }
                 });
 
         }
@@ -49,15 +83,11 @@ export class JointJSElements {
             size: {width: 100, height: 100},
             attrs: {
                 root: {},
-                body: {
-                    fill: 'blue',
-                    rx: 8,
-                    ry: 8,
-                },
+                body: this.blueprintAttrs,
                 label: {
                     text: `${blueprint.getPackageName(0)}\n${blueprint.getShortName()}`,
                     fill: 'white',
-                }
+                },
             },
             ports: {
                 groups: {
@@ -65,23 +95,19 @@ export class JointJSElements {
                         position: {
                             name: "top",
                         },
+                        markup: JointJSElements.inPortMarkup,
                         attrs: {
-                            circle: {
-                                r: 4,
-                                fill: "yellow",
-                            }
-                        }
+                            ".sl-port-in": JointJSElements.inPortAttrs,
+                        },
                     },
                     'MainOut': {
                         position: {
                             name: "bottom",
                         },
+                        markup: JointJSElements.outPortMarkup,
                         attrs: {
-                            circle: {
-                                r: 4,
-                                fill: "cyan",
-                            }
-                        }
+                            ".sl-port-out": JointJSElements.outPortAttrs,
+                        },
                     }
                 },
                 items: portItems,
@@ -90,8 +116,7 @@ export class JointJSElements {
     }
 
     public static createOperatorElement(operator: OperatorModel): dia.Element {
-        return JointJSElements.createBlueprintElement(operator.getBlueprint());
-
+        return JointJSElements.createBlueprintElement(operator.getBlueprint()).attr({body: this.operatorAttrs});
     }
 }
 
