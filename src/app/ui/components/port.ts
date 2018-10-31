9beb680256d72de07c4256c22f1a1fa9fa589110
Julian Matschinske <julian.matschinske@bitspark.de>
2018-10-31 12:40:13 +0100

    Add spacing between ports

--- src/app/ui/components/port.ts
+++ src/app/ui/components/port.ts
@@ -14,6 +14,10 @@ export class PortComponent implements dia.Element.Port {
     public readonly group: string;
     public readonly attrs: dia.Cell.Selectors;
 
+    // Careful: For some reason we must not add any properties other than the above to this class
+    // Otherwise we get performance issues
+    // TODO: Investigate
+
     constructor(port: PortModel, group: string) {
         this.id = `${port.getIdentity()}`;
         this.group = group;
@@ -22,7 +26,7 @@ export class PortComponent implements dia.Element.Port {
         };
     }
 
-    // STATIC
+    // STATIC ONLY:
 
     private static getPortAttributes(group: string, direction: boolean): attributes.SVGAttributes {
         const attrs: attributes.SVGAttributes = {
@@ -51,51 +55,71 @@ export class PortGroupComponent implements dia.Element.PortGroup {
     public markup?: string;
     public attrs?: dia.Cell.Selectors;
 
+    // Careful: For some reason we must not add any properties other than the above to this class
+    // Otherwise we get performance issues
+    // TODO: Investigate
+
     constructor(portOwner: PortOwner, direction: PortDirection, start: number, width: number) {
         switch (direction) {
             case PortDirection.In:
                 if (portOwner instanceof BlackBox) {
                     this.position = PortGroupComponent.layoutFunction("top", start, width) as any;
-                    //this.position = {name: "top"};
                     this.markup = "<path class='sl-srv-main sl-port sl-port-in' d=''></path>";
                     this.attrs = {
-                        ".sl-srv-main.sl-port": PortGroupComponent.portAttrs,
+                        ".sl-srv-main.sl-port": PortGroupComponent.portAttributes,
                     };
                 } else if (portOwner instanceof GenericDelegateModel) {
                     this.position = PortGroupComponent.layoutFunction("right", start, width) as any;
-                    //this.position = {name: "right"};
                     this.markup = "<path class='sl-dlg sl-port sl-port-in' d=''></path>";
                     this.attrs = {
-                        ".sl-dlg.sl-port": PortGroupComponent.portAttrs,
+                        ".sl-dlg.sl-port": PortGroupComponent.portAttributes,
                     };
                 }
                 break;
             case PortDirection.Out:
                 if (portOwner instanceof BlackBox) {
                     this.position = PortGroupComponent.layoutFunction("bottom", start, width) as any;
-                    //this.position = {name: "bottom"};
                     this.markup = "<path class='sl-srv-main sl-port sl-port-out' d=''></path>";
                     this.attrs = {
-                        ".sl-srv-main.sl-port": PortGroupComponent.portAttrs,
+                        ".sl-srv-main.sl-port": PortGroupComponent.portAttributes
                     };
                 } else if (portOwner instanceof GenericDelegateModel) {
                     this.position = PortGroupComponent.layoutFunction("right", start, width) as any;
-                    //this.position = {name: "right"};
                     this.markup = "<path class='sl-dlg sl-port sl-port-out' d=''></path>";
                     this.attrs = {
-                        ".sl-dlg.sl-port": PortGroupComponent.portAttrs,
+                        ".sl-dlg.sl-port": PortGroupComponent.portAttributes,
                     };
                 }
                 break;
         }
     }
 
-    private static portAttrs: attributes.SVGAttributes = {
+    // STATIC ONLY:
+
+    /**
+     * Spacing between ports. Two ports must not be closer to each other than this value.
+     */
+    private static readonly portSpacing = 15;
+    
+    /**
+     * Width of the visible port shape.
+     */
+    private static readonly portWidth = 10;
+    
+    /**
+     * Height of the visible port shape.
+     */
+    private static readonly portHeight = 8;
+
+    /**
+     * SVG attributes for the port shape.
+     */
+    private static readonly portAttributes: attributes.SVGAttributes = {
         paintOrder: "stroke fill",
-        d: "M 0 0 L 10 0 L 5 8 z",
+        d: `M 0 0 L ${PortGroupComponent.portWidth} 0 L ${PortGroupComponent.portWidth / 2} ${PortGroupComponent.portHeight} z`,
         magnet: true,
-        stroke: "black",
-        strokeWidth: 1,
+        stroke: "gray",
+        strokeWidth: 3
     };
 
     private static layoutFunction(position: "top" | "bottom" | "left" | "right", offset: number, space: number): (ports: Array<any>, elBBox: g.Rect, opt: any) => Array<g.Point> {
@@ -114,14 +138,18 @@ export class PortGroupComponent implements dia.Element.PortGroup {
                         total = elBBox.height;
                         break;
                 }
-                
-                const portSpaceAbs = 7.5;
-                const lengthAbs = count * portSpaceAbs;
+
+                const portSpaceAbs = PortGroupComponent.portWidth;
+                const lengthAbs = 
+                    count * portSpaceAbs + 
+                    (count - 1) * PortGroupComponent.portSpacing;
                 const spaceAbs = space * total;
                 const offsetAbs = offset * total;
+                const positionAbs = 
+                    offsetAbs + 
+                    index * (portSpaceAbs + PortGroupComponent.portSpacing) +
+                    (spaceAbs - lengthAbs) / 2;
 
-                const positionAbs = offsetAbs + index * portSpaceAbs + (spaceAbs - lengthAbs) / 2;
-                
                 switch (position) {
                     case "top":
                         return new g.Point(positionAbs, 0);
