2826242317865a18512a845ae1970bcdbfe255e1
Taleh Didover <taleh.didover@bitspark.de>
2018-11-14 18:12:12 +0100

    Prepare building WhiteBox.Component and move some functions from BlueprintView to WhiteBox.Component

--- src/app/ui/components/whitebox.ts
+++ src/app/ui/components/whitebox.ts
@@ -1,14 +1,13 @@
 import {AnchorComponent} from "./anchor";
 import {Styles} from "../../../styles/studio";
 import {BlackBox} from "../../custom/nodes";
-import {dia, shapes} from "jointjs";
+import {dia, g, layout, shapes} from "jointjs";
+import {BlueprintView} from "../views/blueprint";
+import {BlueprintModel} from "../../model/blueprint";
+import {BlackBoxComponent} from "./blackbox";
 
 export namespace WhiteBox {
 	import RectangleSelectors = shapes.standard.RectangleSelectors;
-
-	export class Component extends AnchorComponent {
-		protected shape: Shape;
-	}
 
 	interface Size {
 		width: number,
@@ -19,6 +18,204 @@ export namespace WhiteBox {
 		id?: string
 		cssClass?: string
 		size: Size
+	}
+
+	interface Inner {
+		operators: Array<BlackBoxComponent>
+		ports: {
+			t: Array<dia.Element>
+			b: Array<dia.Element>
+			l: Array<dia.Element>
+			r: Array<dia.Element>
+		}
+	}
+
+	export class Component extends AnchorComponent {
+		private static readonly padding = 120;
+		private static readonly minimumSpace = 10;
+
+		// move all component specific code from BlueprintView to this class
+		public shape: Shape;
+		protected blueprint: BlueprintModel;
+
+		constructor(private readonly blueprintView: BlueprintView) {
+			super(blueprintView, {x: 0, y: 0});
+			this.blueprint = blueprintView.getBlueprint();
+			const size = {
+				width: Component.padding * 2 + Component.minimumSpace,
+				height: Component.padding * 2 + Component.minimumSpace
+			};
+			this.shape = new WhiteBox.Shape(this.blueprint, size);
+			this.shape.addTo(this.graph);
+		}
+
+		public autoLayout({operators, ports}: Inner) {
+			layout.DirectedGraph.layout(this.graph, {
+				nodeSep: 120,
+				rankSep: 120,
+				edgeSep: 0,
+				rankDir: "TB",
+				resizeClusters: false,
+			});
+
+			let boundingBox = this.graph.getCellsBBox(operators.map(operator => operator.getRectangle()))!;
+
+			operators.forEach(operator => {
+				operator.translate(-(boundingBox.x + boundingBox.width / 2), -(boundingBox.y + boundingBox.height / 2));
+			});
+
+			if (!boundingBox) {
+				boundingBox = new g.Rect({x: 0, y: 0, width: Component.minimumSpace, height: Component.minimumSpace});
+			}
+
+			boundingBox.x -= boundingBox.x + boundingBox.width / 2;
+			boundingBox.y -= boundingBox.y + boundingBox.height / 2;
+
+			// Center ports
+
+			const padding = Component.padding;
+
+			for (const port of ports.t) {
+				port.set({
+					position: {
+						x: boundingBox.x - 50 + boundingBox.width / 2,
+						y: boundingBox.y - 100 - padding,
+					}
+				});
+			}
+
+			for (const port of ports.b) {
+				port.set({
+					position: {
+						x: boundingBox.x - 50 + boundingBox.width / 2,
+						y: boundingBox.y + boundingBox.height + padding,
+					}
+				});
+			}
+
+			const offset = boundingBox.y + (boundingBox.height - ports.r.length * 100) / 2;
+			ports.r.forEach((port, index) => {
+				port.set({
+					position: {
+						x: boundingBox.x + boundingBox.width + padding,
+						y: offset + index * 100,
+					}
+				});
+			});
+		}
+
+		public fitOuter(animation: boolean, {operators, ports}: Inner) {
+			const padding = Component.padding;
+			const currentPosition = this.shape.get("position");
+			const currentSize = this.shape.get("size");
+
+			let newX: number = currentPosition.x + padding;
+			let newY: number = currentPosition.y + padding;
+			let newCornerX: number = currentPosition.x + currentSize.width - 2 * padding;
+			let newCornerY: number = currentPosition.y + currentSize.height - 2 * padding;
+
+			operators.forEach(operator => {
+				const childBBox = operator.getBBox();
+				if (childBBox.x < newX) {
+					newX = childBBox.x;
+				}
+				if (childBBox.y < newY) {
+					newY = childBBox.y;
+				}
+				const corner = childBBox.corner();
+				if (corner.x > newCornerX) {
+					newCornerX = corner.x;
+				}
+				if (corner.y > newCornerY) {
+					newCornerY = corner.y;
+				}
+			});
+
+			const set = {
+				position: {x: 0, y: 0},
+				size: {width: 0, height: 0},
+			};
+
+			set.position.x = newX - padding;
+			set.position.y = newY - padding;
+			set.size.width = newCornerX - newX + 2 * padding;
+			set.size.height = newCornerY - newY + 2 * padding;
+
+			let newPosition = {x: currentPosition.x, y: currentPosition.y};
+			let newSize = {width: currentPosition.width, height: currentPosition.height};
+
+			if (currentPosition.x <= set.position.x && currentPosition.y <= set.position.y) {
+				delete set.position;
+			} else {
+				if (currentPosition.x <= set.position.x) {
+					set.position.x = currentPosition.x;
+				} else if (currentPosition.y <= set.position.y) {
+					set.position.y = currentPosition.y;
+				}
+				const deltaX = currentPosition.x - set.position.x;
+				const deltaY = currentPosition.y - set.position.y;
+				set.size.width = Math.max(set.size.width, currentSize.width + deltaX);
+				set.size.height = Math.max(set.size.height, currentSize.height + deltaY);
+				newPosition = set.position;
+				newSize = set.size;
+			}
+
+			if (set.size.width <= currentSize.width && set.size.height <= currentSize.height) {
+				delete set.size;
+			} else {
+				if (set.size.width <= currentSize.width) {
+					set.size.width = currentSize.width;
+				} else if (set.size.height <= currentSize.height) {
+					set.size.height = currentSize.height;
+				}
+				newSize = set.size;
+			}
+
+			if (!!set.position || !!set.size) {
+				if (!animation) {
+					this.shape.set(set);
+				} else {
+					if (!!set.size) {
+						this.shape.transition("size/height", set.size.height);
+						this.shape.transition("size/width", set.size.width);
+					}
+					if (!!set.position) {
+						this.shape.transition("position/x", set.position.x);
+						this.shape.transition("position/y", set.position.y);
+					}
+				}
+
+				for (const port of ports.t) {
+					const currentPortPosition = port.get("position");
+					port.set({
+						position: {
+							x: currentPortPosition.x,
+							y: newPosition.y - 100,
+						}
+					});
+				}
+
+				for (const port of ports.b) {
+					const currentPortPosition = port.get("position");
+					port.set({
+						position: {
+							x: currentPortPosition.x,
+							y: newPosition.y + newSize.height,
+						}
+					});
+				}
+
+				ports.r.forEach(port => {
+					const currentPortPosition = port.get("position");
+					port.set({
+						position: {
+							x: newPosition.x + newSize.width,
+							y: currentPortPosition.y,
+						}
+					});
+				});
+			}
+		}
 	}
 
 	function constructRectAttrs({id, cssClass, size}: BasicAttrs): dia.Element.GenericAttributes<RectangleSelectors> {
