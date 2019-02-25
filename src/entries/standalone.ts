860deba9a6e55c391ef74aab5429c7551c13295c
Taleh Didover <taleh.didover@bitspark.de>
2019-02-25 11:45:19 +0100

    Implement blueprint exporter

--- src/entries/standalone.ts
+++ src/entries/standalone.ts
@@ -4,6 +4,7 @@ import "../styles/standalone.scss";
 
 import {AutoTriggerApp} from "../apps/autotrigger/src/app";
 import {DeploymentApp} from "../apps/deployment/src/app";
+import {BlueprintExporterApp} from "../apps/exporter/src/app";
 import {OperatorDataApp} from "../apps/operators/src/app";
 import {RouterApp} from "../apps/router/src/app";
 import {APIStorageApp} from "../apps/storage/src/app";
@@ -24,6 +25,7 @@ function slangStudioStandalone(el: HTMLElement): Promise<void> {
 		new DeploymentApp(appModel, SLANG_ASPECTS, "http://localhost:5149/");
 		new OperatorDataApp(appModel, SLANG_ASPECTS, COMPONENT_FACTORY);
 		new AutoTriggerApp(appModel, SLANG_ASPECTS);
+		new BlueprintExporterApp(appModel, SLANG_ASPECTS);
 
 		app.load().then(() => {
 			const router = new RouterApp(appModel, SLANG_ASPECTS);
