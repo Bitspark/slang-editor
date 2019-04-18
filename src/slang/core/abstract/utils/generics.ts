baf17fc6a45b9a111607788f05b45129f5b46899
Julian Matschinske <julian.matschinske@bitspark.de>
2019-04-18 11:25:26 +0200

    Rename function and member variable

--- src/slang/core/abstract/utils/generics.ts
+++ src/slang/core/abstract/utils/generics.ts
@@ -78,7 +78,7 @@ export class GenericSpecifications {
 		if (!portSet) {
 			return null;
 		}
-		let unifiedType = this.get(identifier).getManualParts();
+		let unifiedType = this.get(identifier).getFixedParts();
 		for (const registeredPort of portSet) {
 			unifiedType = unifiedType.union(registeredPort.getConnectedType());
 		}
