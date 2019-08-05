90ab7852c13f6cc79d53cf2d04243b9a31d0edea
Julian Matschinske <julian.matschinske@bitspark.de>
2019-08-05 14:55:04 +0200

    Add regression test

--- test/behavior/landscape.test.ts
+++ test/behavior/landscape.test.ts
@@ -60,6 +60,81 @@ describe("The landscape", () => {
 		expect(bp).toEqual(bp2);
 	});
 
+	it("can load a bundle with existing bps", () => {
+		const bundle1: SlangBundle = {
+			main: "a4df2ce5-bf32-4be3-b0a3-85411ec699d4",
+			blueprints: {
+				"a4df2ce5-bf32-4be3-b0a3-85411ec699d4": {
+					id: "a4df2ce5-bf32-4be3-b0a3-85411ec699d4",
+					meta: {
+						name: "Test Blueprint",
+					},
+					services: {
+						main: {
+							in: {
+								type: "string",
+							},
+							out: {
+								type: "number",
+							},
+						},
+					},
+				},
+			},
+		};
+
+		const bundle2: SlangBundle = {
+			main: "27cfa63a-f2e3-4cb1-8b92-d709064214a9",
+			blueprints: {
+				"a4df2ce5-bf32-4be3-b0a3-85411ec699d4": {
+					id: "a4df2ce5-bf32-4be3-b0a3-85411ec699d4",
+					meta: {
+						name: "Test Blueprint",
+					},
+					services: {
+						main: {
+							in: {
+								type: "string",
+							},
+							out: {
+								type: "number",
+							},
+						},
+					},
+				},
+				"27cfa63a-f2e3-4cb1-8b92-d709064214a9": {
+					id: "27cfa63a-f2e3-4cb1-8b92-d709064214a9",
+					meta: {
+						name: "Test Blueprint 2",
+					},
+					services: {
+						main: {
+							in: {
+								type: "boolean",
+							},
+							out: {
+								type: "trigger",
+							},
+						},
+					},
+					operators: {
+						a: {
+							operator: "a4df2ce5-bf32-4be3-b0a3-85411ec699d4",
+							generics: {},
+							properties: {},
+						},
+					},
+				},
+			},
+		};
+
+		landscapeModel.loadBundle(bundle1);
+		const bp = landscapeModel.loadBundle(bundle2);
+
+		expect(landscapeModel.findBlueprint("27cfa63a-f2e3-4cb1-8b92-d709064214a9")).toBeTruthy();
+		expect(bp.name).toEqual("Test Blueprint 2");
+	});
+
 	it("can export a bundle", () => {
 		const bundle = landscapeModel.exportBundle("8019ef19-94c1-46d4-9a34-6dcd4a5281a8");
 		expect(Object.keys(bundle.blueprints)).toEqual(["8019ef19-94c1-46d4-9a34-6dcd4a5281a8", "ba24c37f-2b04-44b4-97ad-fd931c9ab77b"]);
