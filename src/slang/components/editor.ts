f596e26e315754589bbf5d43007317528d6810fb
Taleh Didover <taleh.didover@bitspark.de>
2019-08-05 22:45:06 +0200

    Introduce lit-element from polymer

---
+++ src/slang/components/editor.ts
@@ -0,0 +1,48 @@
+// tslint:disable-next-line:no-unused
+import {customElement, LitElement} from "lit-element";
+import {SlangAspects} from "slang/aspects";
+import {AppModel} from "slang/core/models/app";
+import {Slang} from "slang/slang";
+import {ViewFrame} from "slang/ui/frame";
+
+import {INDEX} from "../../styles";
+
+export * from "slang/definitions/api";
+export * from "slang/definitions/type";
+export * from "slang/core/mapper";
+export * from "slang/core/models/app";
+export * from "slang/core/models/blueprint";
+export * from "slang/core/models/delegate";
+export * from "slang/core/models/landscape";
+export * from "slang/core/models/operator";
+export * from "slang/core/models/port";
+
+@customElement("slang-editor")
+export class SlangEditor extends LitElement {
+	public readonly app: Slang;
+	public readonly appModel: AppModel;
+
+	private readonly frame: HTMLDivElement;
+
+	constructor() {
+		super();
+
+		const shadow = this.attachShadow({mode: "open"});
+
+		this.frame = document.createElement("div");
+		const style = document.createElement("style");
+		style.innerText = INDEX;
+		shadow.appendChild(style);
+		shadow.appendChild(this.frame);
+
+		this.appModel = AppModel.create("slang");
+
+		this.app = new Slang(this.appModel);
+		this.app.addFrame(new ViewFrame(this.frame, new SlangAspects()), true);
+		this.app.load();
+	}
+
+	public get model(): AppModel {
+		return this.appModel;
+	}
+}
