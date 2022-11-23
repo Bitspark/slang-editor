60971e1db115639e069f095ea7182db097803f59
Taleh Didover <dydooov+github@gmail.com>
2022-11-23 16:37:10 +0100

    Fix update behaviour of Icon component

--- src/slang/ui/toolkit/icons.ts
+++ src/slang/ui/toolkit/icons.ts
@@ -7,7 +7,26 @@ export interface IconAttrs extends HasSizeAttrs {
 }
 
 export class Icon implements ClassComponent<IconAttrs> {
+	// @ts-ignore
+	public onupdate(vnode: m.VnodeDOM<IconAttrs, this>): any {
+	}
+
+	// @ts-ignore
+	public onbeforeupdate(vnode: m.Vnode<IconAttrs, this>, old: m.VnodeDOM<IconAttrs, this>): boolean | void {
+		const newAttrs = vnode.attrs
+		const oldAttrs = old.attrs
+
+		if (newAttrs.fas === oldAttrs.fas) {
+			return
+		}
+
+		if (old.dom.firstElementChild) {
+			old.dom.firstElementChild!.remove();
+		}
+	}
+
 	public view({attrs}: CVnode<IconAttrs>) {
-		return m(".span.icon", {class: buildCssClass(attrs)}, m(`.fas.fa-${attrs.fas}`));
+		//return m(".span.icon", {class: buildCssClass(attrs)}, m(`i.fas.fa-${attrs.fas}`));
+		return m(".span.icon", {class: buildCssClass(attrs)}, m.trust(`<i class="fas fa-${attrs.fas}"></i>`));
 	}
 }
