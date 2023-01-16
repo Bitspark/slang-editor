907379355f7caf376b8a9060508a36477d391f8c
Taleh Didover <dydooov+github@gmail.com>
2023-01-16 12:48:45 +0100

    Simplify SlangAspects

--- src/slang/aspects.ts
+++ src/slang/aspects.ts
@@ -1,25 +1,5 @@
-import {BlueprintModel} from "./core/models";
 import {ComponentFactory} from "./ui/factory";
-import {Canvas} from "./ui/canvas/base";
-
-export interface BlueprintToolBoxType {
-	label: string;
-	icon?: string
-	class?: string;
-
-	onclick?(): void;
-}
 
 export class SlangAspects {
 	public readonly factory = new ComponentFactory();
-
-	private aspectBlueprintToolbox = new Array<(view: Canvas, blupr: BlueprintModel, redraw: () => void) => BlueprintToolBoxType[]>();
-
-	public getBlueprintToolboxButtons(view: Canvas, blueprint: BlueprintModel, redraw: () => void): BlueprintToolBoxType[] {
-		return this.aspectBlueprintToolbox.reduce((accumBtnAttrs: BlueprintToolBoxType[], cBtnAttrs) => accumBtnAttrs.concat(cBtnAttrs(view, blueprint, redraw)), []);
-	}
-
-	public registerBlueprintToolboxButton(f: (view: Canvas, blupr: BlueprintModel, redraw: () => void) => BlueprintToolBoxType[]) {
-		this.aspectBlueprintToolbox.push(f);
-	}
 }
